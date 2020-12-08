755f80d527a80928b85f33b11570fb796c480b71
Jesse Pritchard <69367971+Driventobraise@users.noreply.github.com>
2020-12-08 13:26:28 -0800

    math method assignment

---
+++ Math_Function/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Function
+{
+    public class Math
+    {
+        // Three math methods
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Div(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Math_Function/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Asking user for input
+            Console.WriteLine("Please Enter An Integer: ");
+            int userInteger = Convert.ToInt32(Console.ReadLine());
+
+            // instantiating the Math object
+            Math math = new Math();
+            //Calling the Class Maths methods
+            int x = math.Add(userInteger, 5);
+            int y = math.Mult(userInteger, x);
+            int z = math.Div(y, userInteger);
+
+            Console.WriteLine(userInteger + " + " + "5" + " = " + x);
+            Console.WriteLine(userInteger + " * " + x + " = " + y);
+            Console.WriteLine(y + " / " + userInteger + " = " + z);
+            Console.ReadLine();
+        }
+    }
+}
