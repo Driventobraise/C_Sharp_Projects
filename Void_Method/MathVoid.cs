1f901338286fcb1fdc3ba811aceff64b0acf8a3d
Jesse Pritchard <69367971+Driventobraise@users.noreply.github.com>
2020-12-09 10:12:30 -0800

    void method

---
+++ Void_Method/MathVoid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class MathVoid
+    {
+        public void MyMath(int x, int y)
+        {
+            int z = x *10;
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Void_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathVoid math = new MathVoid();
+            math.MyMath(12, 10);
+            
+        }
+    }
+}
