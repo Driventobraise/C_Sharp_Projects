13d40973e66ced93ef21db2568fa08b957936791
Jesse Pritchard <69367971+Driventobraise@users.noreply.github.com>
2020-12-08 18:00:11 -0800

    Optional Parameters

---
+++ Optional_Parameters/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters
+{
+    public class Math
+    {
+        //method with optional parameters
+        public int Divide(int x, int y = 2)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Optional_Parameters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            Math math = new Math();
+
+            //User Input
+            Console.WriteLine("Enter a number: ");
+            int userInt1 = Convert.ToInt32(Console.ReadLine());
+
+            //handling optional user input
+            Console.WriteLine("(OPTIONAL)Enter a second number: ");
+            int userInt2;
+            int result;
+            if (int.TryParse(Console.ReadLine(), out userInt2))
+            {
+                result = math.Divide(userInt1, userInt2);
+            }
+            else
+            {
+                result = math.Divide(userInt1);
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
