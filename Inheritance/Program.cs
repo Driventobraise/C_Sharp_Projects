ca8192d719c767dcfa7653751fcc3da59fec9073
Jesse Pritchard <69367971+Driventobraise@users.noreply.github.com>
2020-12-09 13:14:30 -0800

    inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate and initialize an Employee object
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            // Calling the superclass method
+            employee.SayName();
+        }
+    }
+}
