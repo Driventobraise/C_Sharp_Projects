55ac3a7d6d282762e5c9f1e7b2f115eff3e8b8d2
Jesse Pritchard <69367971+Driventobraise@users.noreply.github.com>
2020-12-08 16:51:21 -0800

    method overloading

---
+++ Math_Method_Continued/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Method_Continued
+{
+    public class Math
+    {
+        public int Mult(int x)
+        {
+            return x * 5;
+        }
+
+        public int Mult(decimal x)
+        {
+            return Convert.ToInt32(x * 2.5m);
+        }
+
+        public int Mult(string x)
+        {
+            int z = Convert.ToInt32(x);
+            return z * 10;
+        }
+    }
+}
---
+++ Math_Method_Continued/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Method_Continued
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 6;
+            Math math = new Math();
+            int a = math.Mult(x);
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+            decimal y = 1.2m;
+            int b = math.Mult(y);
+
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+            string z = "5";
+            int c = math.Mult(z);
+
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
