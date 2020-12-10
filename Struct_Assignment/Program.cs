16b5dafcce342b60c077f7fada15d883f4dfcb3a
Jesse Pritchard <69367971+Driventobraise@users.noreply.github.com>
2020-12-10 15:15:00 -0800

    struct

---
+++ Struct_Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    class Program
+    {
+        struct Number
+        {
+            public decimal Amount;
+        };
+
+        static void Main(string[] args)
+        {
+            Number number;
+            number.Amount = 2.5m;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
