fe362139002982838fe56722408e35f52e098d3a
Jesse Pritchard <69367971+Driventobraise@users.noreply.github.com>
2020-12-14 17:13:52 -0800

    additional features

---
+++ ADDITIONAL_FEATURES/ChainConstructors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADDITIONAL_FEATURES
+{
+    public class ChainConstructors
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        //Chaining two constructors together.
+        public ChainConstructors(string name) : this(name, 10) { }
+        public ChainConstructors(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ ADDITIONAL_FEATURES/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADDITIONAL_FEATURES
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //const variable.
+            const string schoolName = "The Tech Acadamy";
+
+            //Creating a variable using the keyword "var.”
+            var employeeList = new List<Tuple<string, string, int>>();
+            employeeList.Add(Tuple.Create("Joe", "Bob", 1));
+            foreach (var employee in employeeList)
+            {
+                string s = employee.Item1;
+                string s2 = employee.Item2;
+                int i = employee.Item3;
+                //displaying constant variable and "var" utilization.
+                Console.WriteLine("{0} {1} goes to {2}.", s, s2, schoolName);
+                Console.ReadLine();
+            }
+            //Chaining two constructors together.
+            ChainConstructors person = new ChainConstructors("Jesse");
+
+            Console.WriteLine(Convert.ToString(person.Age));
+            Console.WriteLine(person.Name);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
